8f4014f747cc50a674e7190e3267b19fb2b97e56
Sima Nerush <2002ssn@gmail.com>
2021-10-26 15:19:37 -0700

    Initial commit, set up Podfile and Info.plist

---
+++ GoogleAd/NewsTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  NewsTableViewController.swift
+//  GoogleAdDemo
+//
+//  Created by Simon Ng on 29/10/2020.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows
+        return 4
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsTableViewCell
+        
+        // Configure the cell...
+        if indexPath.row == 0 {
+            cell.postImageView.image = UIImage(named: "red-lights-lisbon")
+            cell.postTitle.text = "Red Lights, Lisbon"
+            cell.postAuthor.text = "TOM EVERSLEY (@tomeversley)"
+            cell.authorImageView.image = UIImage(named: "appcoda-fav-logo")
+            
+        } else if indexPath.row == 1 {
+            cell.postImageView.image = UIImage(named: "val-throrens-france")
+            cell.postTitle.text = "Val Thorens, France"
+            cell.postAuthor.text = "BARA ART (bara-art.com)"
+            cell.authorImageView.image = UIImage(named: "appcoda-fav-logo")
+            
+        } else if indexPath.row == 2 {
+            cell.postImageView.image = UIImage(named: "summer-beach-huts")
+            cell.postTitle.text = "Summer Beach Huts, England"
+            cell.postAuthor.text = "TOM EVERSLEY (@tomeversley)"
+            cell.authorImageView.image = UIImage(named: "appcoda-fav-logo")
+            
+        } else {
+            cell.postImageView.image = UIImage(named: "taxis-nyc")
+            cell.postTitle.text = "Taxis, NYC"
+            cell.postAuthor.text = "TOM EVERSLEY (@tomeversley)"
+            cell.authorImageView.image = UIImage(named: "appcoda-fav-logo")
+            
+        }
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
